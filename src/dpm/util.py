c63eb2db61bfa044b77df9073fc2da176ea21a7a
Revolving Madness <andyallen216@gmail.com>
2023-03-18 20:03:29 -0500

    Modified .gitignore

---
+++ src/dpm/util.py
@@ -0,0 +1,24 @@
+def to_sentence_case(sentence):
+    return sentence.title().replace('-', ' ').replace('_', ' ')
+
+
+def remove_file_extension(file_name):
+    return '.'.join(file_name.split('.')[:-1])
+
+def json_to_dependencies(json):
+    from pyrinth.modrinth import Modrinth
+    result = []
+
+    for name, dep_type in json.items():
+        project = Modrinth.get_project(name)
+        if not project:
+            print(f"Dependency '{name}' not found")
+            return None
+        project_id = project.get_id()
+        dep = {
+            "project_id": project_id,
+            "dependency_type": dep_type
+        }
+        result.append(dep)
+
+    return result
