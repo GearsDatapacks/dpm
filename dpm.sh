506c9d437e0c1fb7adb349cb2e735768b7755823
GearsDatapacks <surya@seriousinfinitude.com>
2023-08-27 16:37:43 +0100

    fix: delete leftover files from python version

--- dpm.sh
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/bash
-
-DIR=$(pwd)
-
-cd ~/.local/share/dpm/src/dpm
-
-python dpm.py $@ --dir "$DIR"
--- install.sh
+++
@@ -1,6 +0,0 @@
-mkdir -p ~/.local/share/dpm
-rm -rf ~/.local/share/dpm/src
-cp -r ./src ~/.local/share/dpm
-pip install pyrinth
-sudo cp ./dpm.sh /usr/bin/dpm
-sudo chmod +x /usr/bin/dpm
