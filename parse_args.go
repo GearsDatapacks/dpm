25374a3aeb9ef3e4930ddddf291ef84c593741e2
GearsDatapacks <surya@seriousinfinitude.com>
2023-12-27 19:01:21 +0000

    ref: make actions data-driven in parse_args

--- parse_args.go
+++ parse_args.go
@@ -12,26 +12,35 @@ type parsedArgs struct {
 	values map[string]string
 }
 
+var actions = map[string]int{
+	"install":   -1,
+	"uninstall": -1,
+	"publish":   0,
+	"init":      0,
+	"alias":     2,
+	"create":    2,
+	"rm-alias":  1,
+}
 var flagArgs = []string{"help", "version", "dev", "optional"}
 var valueArgs = []string{"auth"}
 var aliasEntries = map[string][]string{
-	"uninstall": {"remove","ui"},
-	"install": {"i"},
-	"init":    {"initialise", "initialize"},
+	"uninstall": {"remove", "ui"},
+	"install":   {"i"},
+	"init":      {"initialise", "initialize"},
 }
-var aliases = map[string]string{}
+var aliases = formatAliases(aliasEntries)
 
-func formatAliases() {
-	for command, aliasArray := range aliasEntries {
+func formatAliases(unformatted map[string][]string) map[string]string {
+	aliases := map[string]string{}
+	for command, aliasArray := range unformatted {
 		for _, alias := range aliasArray {
 			aliases[alias] = command
 		}
 	}
+	return aliases
 }
 
 func parseArgs(args []string) parsedArgs {
-	formatAliases()
-
 	result := parsedArgs{
 		values: map[string]string{},
 	}
@@ -43,85 +52,55 @@ func parseArgs(args []string) parsedArgs {
 			arg = alias
 		}
 
-		if arg == "install" {
-			result.action = "install"
-			result.data = []string{}
-
-			for (i+1) < len(args) && !strings.HasPrefix(args[i+1], "-") {
-				result.data = append(result.data, args[i+1])
-				i++
-			}
-		} else if arg == "uninstall" {
-			result.action = "uninstall"
-			result.data = []string{}
-
-			for (i+1) < len(args) && !strings.HasPrefix(args[i+1], "-") {
-				result.data = append(result.data, args[i+1])
-				i++
-			}
-		} else if arg == "publish" {
-			result.action = "publish"
-		} else if arg == "init" {
-			result.action = "init"
-		} else if arg == "alias" {
-			result.action = "alias"
-			result.data = []string{}
-
-			if i+2 >= len(args) {
-				if i+1 < len(args) && strings.HasPrefix(args[i+1], "--") {
-					continue
-				}
-
-				log.Fatal("Subcommand 'alias' takes two arguments")
-			}
-
-			result.data = append(result.data, args[i+1], args[i+2])
-
-			i += 2
-		} else if arg == "create" {
-			result.action = "create"
-			result.data = []string{}
-
-			if i+2 >= len(args) {
-				if i+1 < len(args) && strings.HasPrefix(args[i+1], "--") {
-					continue
-				}
-
-				log.Fatal("Subcommand 'create' takes two arguments")
-			}
-
-			result.data = append(result.data, args[i+1], args[i+2])
-
-			i += 2
-		} else if arg == "rm-alias" {
-			result.action = "rm-alias"
-			result.data = []string{}
-			if i+1 >= len(args) {
-				log.Fatal("Subcommand 'rm-alias' takes one argument")
-			}
-
-			if strings.HasPrefix(args[i+1], "--") {
-				continue
-			}
-			
-			result.data = append(result.data, args[i+1])
-
-			i++
-		} else if strings.HasPrefix(arg, "--") {
+		if strings.HasPrefix(arg, "--") {
 			argName := arg[2:]
 
 			if contains(flagArgs, argName) {
 				result.flags = append(result.flags, argName)
-			} else if contains(valueArgs, argName) {
-				i++
-				result.values[argName] = args[i]
+				} else if contains(valueArgs, argName) {
+					i++
+					result.values[argName] = args[i]
 			} else {
 				log.Fatalf("Unexpected argument %q", arg)
 			}
-		} else {
+			continue
+		}
+		
+		argCount, ok := actions[arg]
+		if !ok {
 			log.Fatalf("Unexpected argument %q", arg)
 		}
-	}
+		
+		if result.action != "" {
+			log.Fatalf("Unexpected argument %q", arg)
+		}
+		result.action = arg
+		result.data = []string{}
+		
+		if argCount == 0 {
+			continue
+		}
 
+		if argCount == -1 {
+			for (i+1) < len(args) && !strings.HasPrefix(args[i+1], "--") {
+				result.data = append(result.data, args[i+1])
+				i++
+			}
+			continue
+		}
+
+		if (i+1) < len(args) && strings.HasPrefix(args[i+1], "--") {
+			continue
+		}
+
+		for count := 0; count < argCount; count++ {
+			i++
+			if i >= len(args) || strings.HasPrefix(args[i], "--") {
+				log.Fatalf("Subcommand %q takes %d arguments", arg, argCount)
+			}
+			result.data = append(result.data, args[i])
+		}
+		continue
+	}
 	return result
 }
